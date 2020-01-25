38fe96250dddd32f448457c380c513e082a7a36b
Moesubs <59392472+Moesubs@users.noreply.github.com>
2020-01-25 15:52:09 +0800

    Add files via upload

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/moesubs/live2d/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/moesubs/live2d1/";
 //const live2d_path = "/live2d/";
 
 // 封装异步加载资源的方法
@@ -30,7 +30,7 @@ Promise.all([
 	loadExternalResource(live2d_path + "live2d.min.js", "js"),
 	loadExternalResource(live2d_path + "waifu-tips.js", "js")
 ]).then(() => {
-	initWidget(live2d_path + "waifu-tips.json", "https://live2d.moesubs.net/api");
+	initWidget(live2d_path + "waifu-tips.json", "https://live2d.fghrsh.net/api");
 });
 // initWidget 第一个参数为 waifu-tips.json 的路径，第二个参数为 API 地址
 // API 后端可自行搭建，参考 https://github.com/fghrsh/live2d_api
